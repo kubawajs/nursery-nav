fd3a9746e348b33858340a721d64bb35b54afb20
Kuba <jakub.wajs10@gmail.com>
2024-03-04 09:26:37 +0100

    #7 Prepare test data set (#8)
    * Add JSON with test data
    
    * Add nursery test data + interface

---
+++ src/nursery-nav/src/shared/nursery.interface.ts
@@ -0,0 +1,40 @@
+export interface MapComponentProps {
+    institutionType: string,
+    name: string,
+    website: string,
+    email: string,
+    phone: string,
+    capacity: number,
+    kindsEnrolled: number,
+    basicPricePerMonth: number,
+    extendedStayOver10H?: number,
+    basicPricePerHour?: number,
+    foodPricePerMonth?: number,
+    foodPricePerDay?: number,
+    discounts: string[],
+    openingHours: string,
+    isAdaptedToDisabledChildren: boolean,
+    operatingEntity: OperatingEntity,
+    businessActivitySuspended: boolean,
+    address: Address
+}
+
+export interface OperatingEntity {
+    name: string,
+    address: string,
+    nip: string,
+    regon: string,
+    regNoPosition: string,
+    website: string
+}
+
+export interface Address {
+    voidodeship: string,
+    county: string,
+    city: string,
+    fullAddress: string,
+    pin: {
+        latitude: number,
+        longitude: number
+    }
+}
