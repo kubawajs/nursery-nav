9ccfeafd55685b5226aee9d3c43c0e6126304260
kubawajs <jakub.wajs10@gmail.com>
2024-03-25 00:02:27 +0100

    Add filtering by city and institution type

--- src/nursery-nav/src/components/Filters/Filters.tsx
+++ src/nursery-nav/src/components/Filters/Filters.tsx
@@ -1,10 +1,13 @@
 import { Autocomplete, TextField, FormGroup, FormControlLabel, Checkbox } from "@mui/material";
 import RangeSlider from "./RangeSlider";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { InstitutionContext } from "../../App";
 
 export default function Filters() {
-    const { institutions, setSelectedInstitution } = useContext(InstitutionContext);
+    const { institutions, setFilteredInstitutions, setSelectedInstitution } = useContext(InstitutionContext);
+    const [cityFilter, setCityFilter] = useState<string | null>(null);
+    const [nurseryFilter, setNurseryFilter] = useState<boolean>(true);
+    const [childClubFilter, setChildClubFilter] = useState<boolean>(true);
 
     function setCurrentSelection(event: any, value: any) {
         const currentSelection = institutions.find(institution => institution.name === value);
@@ -12,6 +15,13 @@ export default function Filters() {
             setSelectedInstitution(currentSelection);
         }
     }
+
+    useEffect(() => {
+        setFilteredInstitutions(institutions.filter(institution =>
+            (!cityFilter || institution.address.city === cityFilter) &&
+            (nurseryFilter && institution.institutionType === 'Żłobek' || childClubFilter && institution.institutionType === 'Klub dziecięcy')
+        ));
+    }, [cityFilter, nurseryFilter, childClubFilter]);
 
     return (
         <>
@@ -24,18 +34,20 @@ export default function Filters() {
                 sx={{ width: 400, maxWidth: '100%' }}
             />
             <Autocomplete
-                options={institutions.map(institution => institution.address.city)}
+                id="cityFilter"
+                options={institutions.map(institution => institution.address.city).filter((value, index, self) => self.indexOf(value) === index)}
+                onChange={(_event, value) => setCityFilter(value)}
                 renderInput={(params) => <TextField {...params} label="Miasto" />}
                 size="small"
                 sx={{ width: 300, maxWidth: '100%' }}
             />
-            <FormGroup sx={{ display: 'flex', flexDirection: 'row' }}>
+            <FormGroup sx={{ display: 'flex', flexDirection: 'row' }} >
                 <FormControlLabel
-                    control={<Checkbox defaultChecked />}
+                    control={<Checkbox defaultChecked onChange={(_event, value) => setNurseryFilter(value)} />}
                     label="Żłobek"
                 />
                 <FormControlLabel
-                    control={<Checkbox defaultChecked />}
+                    control={<Checkbox defaultChecked onChange={(_event, value) => setChildClubFilter(value)} />}
                     label="Klub dziecięcy"
                 />
             </FormGroup>
--- src/nursery-nav/src/components/ListComponent/ListComponent.tsx
+++ src/nursery-nav/src/components/ListComponent/ListComponent.tsx
@@ -1,15 +1,11 @@
-import {
-	Box,
-	List,
-	Typography
-} from '@mui/material';
+import { Box, List, Typography } from '@mui/material';
 import { useContext } from 'react';
 import { InstitutionContext } from '../../App';
 import { ListComponentItem } from './ListComponentItem';
 import InstitutionDetails from '../InstitutionDetails/InstitutionDetails';
 
 export default function ListComponent() {
-	const { institutions, selectedInstitution, setSelectedInstitution } = useContext(InstitutionContext);
+	const { filteredInstitutions, selectedInstitution, setSelectedInstitution } = useContext(InstitutionContext);
 
 	if (selectedInstitution) {
 		return (
@@ -20,10 +16,10 @@ export default function ListComponent() {
 	return (
 		<Box component="section" style={{ overflow: 'auto', height: '100vh' }}>
 			<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-				Znaleziono {institutions.length} instytucji
+				Znaleziono {filteredInstitutions.length} instytucji
 			</Typography>
 			<List>
-				{institutions.map((institution, index) => (
+				{filteredInstitutions.map((institution, index) => (
 					<Box key={index} onClick={() => setSelectedInstitution(institution)}>
 						<ListComponentItem
 							key={index}
