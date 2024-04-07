f59afafaacbb47b012afb2f0e6c73816b70f23ef
kubawajs <jakub.wajs10@gmail.com>
2024-04-07 22:50:51 +0200

    Add static data source to service

--- src/nursery-nav-api/src/institutions/institutions.service.ts
+++ src/nursery-nav-api/src/institutions/institutions.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { Institution } from './interfaces/institution.interface';
+import { InstitutionType } from './interfaces/institutiontype.interface';
+import data from '../../data/test-data-100.json';
 
 @Injectable()
 export class InstitutionsService {
     findAll(): Promise<Institution[]> {
+        const institutions = data as unknown as Institution[];
+        console.log(data);
+        if (data) {
+            console.log("hello from data");
+            return Promise.resolve(institutions);
+        }
+
         return Promise.resolve([]);
     }
 }
