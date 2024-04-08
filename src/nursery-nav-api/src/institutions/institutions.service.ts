02cdcc4288ba60ac3bf9cb54bd9b58491ebb5605
kubawajs <jakub.wajs10@gmail.com>
2024-04-08 22:41:20 +0200

    Add get by ID endpoint

--- src/nursery-nav-api/src/institutions/institutions.service.ts
+++ src/nursery-nav-api/src/institutions/institutions.service.ts
@@ -10,11 +10,21 @@ export class InstitutionsService {
         this.institutions = data as unknown as Institution[];
     }
 
-    findAll(): Promise<Institution[]> {
+    async findAll(): Promise<Institution[]> {
         if (data) {
             return Promise.resolve(this.institutions);
         }
 
         return Promise.resolve([]);
+    }
+
+    async getById(regNo: string): Promise<Institution> {
+        const institution = this.institutions.find((institution) => institution.operatingEntity.regNoPosition === regNo);
+        console.log(institution);
+        if (institution) {
+            return Promise.resolve(institution);
+        }
+
+        return Promise.reject(`Institution with id ${regNo} not found`);
     }
 }
