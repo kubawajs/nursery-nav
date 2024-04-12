a2e4b917c26e55a2d491e7fb13b12cabbdd0a5e9
kubawajs <jakub.wajs10@gmail.com>
2024-04-12 13:19:35 +0200

    Add CORS to app

--- src/nursery-nav-api/src/main.ts
+++ src/nursery-nav-api/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { env } from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS
+  var corsOptions = {
+    origin: env.FRONTEND_URL,
+  }
+  app.enableCors(corsOptions);
+
+  // Setup Swagger
   const config = new DocumentBuilder()
     .setTitle('Nursery Nav API')
     .setDescription('API for Nursery Nav')
@@ -14,6 +22,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  // Start the app
   await app.listen(3000);
 }
 bootstrap();
