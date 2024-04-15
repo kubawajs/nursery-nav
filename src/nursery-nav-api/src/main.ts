598358f18994db977494e83568cac1a46d391aa0
kubawajs <jakub.wajs10@gmail.com>
2024-04-15 20:18:55 +0200

    Serve Swagger JS and CSS from CDN

--- src/nursery-nav-api/src/main.ts
+++ src/nursery-nav-api/src/main.ts
@@ -20,7 +20,14 @@ async function bootstrap() {
     .addTag('nursery-nav')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.15.2/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.15.2/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.15.2/swagger-ui-standalone-preset.js',
+    ],
+  });
 
   // Start the app
   await app.listen(3000);
