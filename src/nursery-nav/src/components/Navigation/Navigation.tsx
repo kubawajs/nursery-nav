f87a991e8b5034092b4896ae13f943c9a4ccc0f1
kubawajs <jakub.wajs10@gmail.com>
2024-05-03 00:41:17 +0200

    Update readme, add link to homepage on logo click

--- src/nursery-nav/src/components/Navigation/Navigation.tsx
+++ src/nursery-nav/src/components/Navigation/Navigation.tsx
@@ -1,7 +1,4 @@
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
+import { AppBar, Container, Toolbar, Link, Typography } from '@mui/material';
 import { FamilyRestroom } from '@mui/icons-material';
 
 export default function Navigation() {
@@ -9,21 +6,23 @@ export default function Navigation() {
 		<AppBar position="sticky">
 			<Container maxWidth="xl">
 				<Toolbar>
-					<FamilyRestroom sx={{ display: 'flex', mr: 1 }} />
-					<Typography
-						variant="h5"
-						noWrap
-						sx={{
-							mr: 2,
-							display: 'flex',
-							fontFamily: 'monospace',
-							fontWeight: 700,
-							letterSpacing: '.2rem',
-							textDecoration: 'none'
-						}}
-					>
-						{process.env.REACT_APP_NAME}
-					</Typography>
+					<Link href="/" underline="none" color="inherit" display='flex' sx={{ flexGrow: 1 }}>
+						<FamilyRestroom sx={{ display: 'flex', mr: 1 }} />
+						<Typography
+							variant="h5"
+							noWrap
+							sx={{
+								mr: 2,
+								display: 'flex',
+								fontFamily: 'monospace',
+								fontWeight: 700,
+								letterSpacing: '.2rem',
+								textDecoration: 'none'
+							}}
+						>
+							{process.env.REACT_APP_NAME}
+						</Typography>
+					</Link>
 				</Toolbar>
 			</Container>
 		</AppBar>
