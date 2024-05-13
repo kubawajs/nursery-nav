a47c35e8a6a8cda5f14f045428cc9427fd6a404e
kubawajs <jakub.wajs10@gmail.com>
2024-05-13 23:24:44 +0200

    LocationsService caching fix

--- src/nursery-nav-api/src/locations/locations.service.ts
+++ src/nursery-nav-api/src/locations/locations.service.ts
@@ -8,27 +8,26 @@ import { env } from "process";
 @Injectable()
 export class LocationsService {
     private locations: LocationDto[];
-    private CACHE_KEY = 'LocationsService_findAll';
 
     constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {
-        (async () => {
-            this.locations = await this.loadData();
-        })();
+        this.loadData();
     }
 
     async findAll() {
+        const CACHE_KEY = 'LocationsService_findAll';
+        const cacheData = await this.cacheManager.get(CACHE_KEY) as LocationDto[];
+        if (cacheData) {
+            return cacheData;
+        }
+
+        await this.cacheManager.set(CACHE_KEY, this.locations, Number(env.CACHE_TTL));
         return Promise.resolve(this.locations);
     }
 
     private async loadData() {
-        const cacheData = await this.cacheManager.get(this.CACHE_KEY);
-        if (cacheData) {
-            console.log('returning from cache:', this.CACHE_KEY);
-            return cacheData;
-        }
         try {
             const data = require('../../data/052024-RZ-instytucje-enriched.json');
-            const locationsData = data.map((location: {
+            this.locations = data.map((location: {
                 institutionType: InstitutionType;
                 id: number;
                 address: { pin: { longitude: number; latitude: number; }; };
@@ -40,9 +39,6 @@ export class LocationsService {
                     latitude: location.address.pin.latitude
                 };
             });
-            console.log('Saving to cache with key:', this.CACHE_KEY);
-            await this.cacheManager.set('locations_data', locationsData, Number(env.CACHE_TTL));
-            return locationsData;
         } catch (error) {
             console.error('Error loading data:', error);
         }
