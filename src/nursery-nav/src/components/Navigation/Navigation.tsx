4d372ce827072cdd2476d5275cc49339b2532beb
kubawajs <jakub.wajs10@gmail.com>
2024-05-27 22:40:02 +0200

    Only logo in navigation should be clickable

--- src/nursery-nav/src/components/Navigation/Navigation.tsx
+++ src/nursery-nav/src/components/Navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Container, Toolbar, Link, Typography } from '@mui/material';
+import { AppBar, Container, Toolbar, Link, Typography, Box } from '@mui/material';
 import { FamilyRestroom } from '@mui/icons-material';
 
 export default function Navigation() {
@@ -6,23 +6,25 @@ export default function Navigation() {
 		<AppBar position="sticky">
 			<Container maxWidth="xl">
 				<Toolbar>
-					<Link href="/" underline="none" color="inherit" display='flex' sx={{ flexGrow: 1 }}>
-						<FamilyRestroom sx={{ display: 'flex', mr: 1 }} />
-						<Typography
-							variant="h5"
-							noWrap
-							sx={{
-								mr: 2,
-								display: 'flex',
-								fontFamily: 'monospace',
-								fontWeight: 700,
-								letterSpacing: '.1rem',
-								textDecoration: 'none'
-							}}
-						>
-							{process.env.REACT_APP_NAME}
-						</Typography>
-					</Link>
+					<Box>
+						<Link href="/" underline="none" color="inherit" display='flex' sx={{ flexGrow: 1 }}>
+							<FamilyRestroom sx={{ display: 'flex', mr: 1 }} />
+							<Typography
+								variant="h5"
+								noWrap
+								sx={{
+									mr: 2,
+									display: 'flex',
+									fontFamily: 'monospace',
+									fontWeight: 700,
+									letterSpacing: '.1rem',
+									textDecoration: 'none'
+								}}
+							>
+								{process.env.REACT_APP_NAME}
+							</Typography>
+						</Link>
+					</Box>
 				</Toolbar>
 			</Container>
 		</AppBar>
