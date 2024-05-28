33b07fb9a438654c1444b80da3be7a0f83b57166
kubawajs <jakub.wajs10@gmail.com>
2024-05-28 22:09:31 +0200

    Theme update

--- src/nursery-nav/src/shared/theme.tsx
+++ src/nursery-nav/src/shared/theme.tsx
@@ -4,18 +4,23 @@ export const theme = createTheme({
     palette: {
         mode: 'light',
         primary: {
-            light: '#e5f6ff',
-            main: '#98d9ff',
-            dark: '#57a5ec',
+            light: '#e6f3fb',
+            main: '#4580c4',
+            dark: '#315292',
         },
         secondary: {
-            light: '#fbede9',
-            main: '#ffbe98',
-            dark: '#f77c18',
+            light: '#f7e6f0',
+            main: '#c44580',
+            dark: '#6f365d',
+        },
+        success: {
+            light: '#d1e9c8',
+            main: '#4caf50',
+            dark: '#27632a',
         },
         text: {
-            primary: '#333',
-            secondary: '#666',
+            primary: '#0d0c1d',
+            secondary: '#5e5d70',
         },
     },
     spacing: 8,
