f7f44bbedfb75e05136e81bfe1e49192274c0f03
kubawajs <jakub.wajs10@gmail.com>
2025-03-01 23:41:26 +0100

    Add next/fonts

--- src/nursery-nav/src/app/layout.tsx
+++ src/nursery-nav/src/app/layout.tsx
@@ -1,10 +1,20 @@
 import { Metadata } from "next";
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: ["100", "300", "400", "500", "700", "900"],
+})
 
 export const metadata: Metadata = {
     title:
         "ZnajdzZlobek.pl | Darmowa wyszukiwarka żłobków",
     description:
         "Znajdź idealny żłobek dla dziecka w najlepszej cenie PLN na miesiąc. Sprawdź dostępność miejsc i dowiedz się, gdzie ich brak. Poznaj nazwy żłobków w okolicy.",
+};
+
+export const viewport = {
     themeColor: "#98d9ff",
 };
 
@@ -14,14 +24,10 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="pl">
+        <html lang="pl" className={roboto.className}>
             <head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Mynerve&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-                    rel="stylesheet" />
                 <link rel="apple-touch-icon" href="/logo192.png" />
+
             </head>
             <body>
                 <div id="root">{children}</div>
